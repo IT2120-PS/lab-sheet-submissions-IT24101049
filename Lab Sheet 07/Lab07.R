f7a5e930f2598bd212e9f7b175ede13fed2a51cc
IT24101049 <it24101049@my.sliit.lk>
2025-09-16 12:06:04 +0530

    IT24101049Lab07

---
+++ Lab Sheet 07/Lab07.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\MSI GF63\\Desktop\\IT24101049")
+getwd()
+
+##Question 01
+1-pnorm(130,mean = 100, sd=15, lower.tail = TRUE)
+
+##Question 02
+pexp (2, rate =1/3, lower.tail = TRUE)
+
+##Question 03 i)
+1-pnorm(130,mean = 100, sd=15, lower.tail = TRUE)
+
+##Question 03 ii)
+qnorm(0.95,mean = 100, sd=15, lower.tail = TRUE)
+
+
+
+
